ced23aeb25a2555d5d14bc87e4357622c53dd72f
Judah1604 <81442563+Judah1604@users.noreply.github.com>
2022-11-27 00:20:28 +0100

    Delete search.js

--- JS/search.js
+++
@@ -1,8 +0,0 @@
-let searchbar = document.querySelector(".search");
-const google = 'https://www.google.com/search?q=';
-const site = searchbar.textContent;
-
-    function search() {
-        const url = google + site;
-        const win = window.open(url, '_blank');
-    }
