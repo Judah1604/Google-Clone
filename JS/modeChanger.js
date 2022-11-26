a0a351a631a0e2942892f2fbfba4f19ec96ace17
Judah1604 <81442563+Judah1604@users.noreply.github.com>
2022-11-27 00:28:04 +0100

    Update modeChanger.js

--- JS/modeChanger.js
+++ JS/modeChanger.js
@@ -1,13 +1,15 @@
 
+
+let body = document.body;
 let darkbtn = document.querySelector('.darkbtn');
 let slider = document.querySelector('.slider');
-let body = document.body;
 let navtext1 = document.querySelector('.nav1');
 let navtext2 = document.querySelector('.nav2');
+let searchicon = document.querySelector('.searchicon');
 var search = document.querySelector('.search');
 var search_opt = document.querySelector('.search-options');
 var lang = document.querySelector('.languages');
-var footer = document.querySelector('.footer');
+var footer = document.querySelector('footer');
 var footerline2 = document.querySelector('.footerline2');
 
 darkbtn.addEventListener('click', ()=> {
@@ -16,9 +18,11 @@ darkbtn.addEventListener('click', ()=> {
   darkbtn.classList.toggle('active');
   navtext1.classList.toggle('dark-nav');
   navtext2.classList.toggle('dark-nav');
+  searchicon.classList.toggle('icon-dark')
   search.classList.toggle('input-dark');
   search_opt.classList.toggle('options');
   lang.classList.toggle('options');
   footer.classList.toggle('footer-dark');
   footerline2.classList.toggle('footerline-dark');
 });
+
